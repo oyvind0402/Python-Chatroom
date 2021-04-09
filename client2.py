e9a9ea4cb9c486b3e9a6de0272186c52797b17e1
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2021-04-09 15:05:58 +0200

    My work is now in these two new files :D

---
+++ client2.py
@@ -0,0 +1,70 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/api/"
+
+print("-------------------------------------------------------")
+print("RESULT FROM POST A USER")
+response = requests.post(BASE + "user/0", {"username": "oyvind91", "usertype": "admin"})
+print(response.json())
+
+print("-------------------------------------------------------")
+print("RESULT FROM POST A USER")
+response = requests.post(BASE + "user/1", {"username": "someone", "usertype": "regular"})
+print(response.json())
+
+print("-------------------------------------------------------")
+print("RESULT FROM GET A USER")
+response = requests.get(BASE + "user/0")
+print(response.json())
+
+print("-------------------------------------------------------")
+print("RESULT FROM GET A USER")
+response = requests.get(BASE + "user/1")
+print(response.json())
+
+print("-------------------------------------------------------")
+print("RESULT FROM GET ALL USERS")
+response = requests.get(BASE + "users")
+print(response.json())
+
+print("-------------------------------------------------------")
+print("RESULT FROM POST A ROOM")
+response = requests.post(BASE + "room/0", {"roomname": "ObligWork"})
+print(response.json())
+
+print("-------------------------------------------------------")
+print("RESULT FROM POST THE SAME ROOM_ID")
+response = requests.post(BASE + "room/0", {"roomname": "ObligWork"})
+print(response.json())
+
+print("-------------------------------------------------------")
+print("RESULT FROM POST A ROOM")
+response = requests.post(BASE + "room/1", {"roomname": "PortfolioWork"})
+print(response.json())
+
+print("-------------------------------------------------------")
+print("RESULT FROM GET A ROOM")
+response = requests.get(BASE + "room/0")
+print(response.json())
+
+print("-------------------------------------------------------")
+print("RESULT FROM GET ALL ROOMS")
+response = requests.get(BASE + "rooms")
+print(response.json())
+
+
+print("-------------------------------------------------------")
+print("RESULT FROM POST A ROOMUSER")
+response = requests.post(BASE + "room/1/users", {"user_id": 0})
+print(response.json())
+
+print("-------------------------------------------------------")
+print("RESULT FROM POST A ROOMUSER")
+response = requests.post(BASE + "room/1/users", {"user_id": 1})
+print(response.json())
+
+print("-------------------------------------------------------")
+print("RESULT FROM GET ROOMUSERS")
+response = requests.get(BASE + "room/1/users")
+print(response.json())
+
---
+++ main2.py
@@ -0,0 +1,116 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort
+
+app = Flask(__name__)
+api = Api(app)
+
+user_post_args = reqparse.RequestParser()
+user_post_args.add_argument("username", type=str, help="Username is required...", required=True)
+user_post_args.add_argument("usertype", type=str, help="Invalid syntax for usertype...", required=False)
+
+room_post_args = reqparse.RequestParser()
+room_post_args.add_argument("roomname", type=str, help="Roomname is required...", required=True)
+
+room_user_post_args = reqparse.RequestParser()
+room_user_post_args.add_argument("user_id", type=list)
+
+message_post_args = reqparse.RequestParser()
+message_post_args.add_argument("message", type=str, help="Message is required...", required=True)
+
+
+users = {}
+rooms = {}
+roomusers = {}
+messages = {}
+
+
+def abort_if_user_not_exists(user_id):
+    if user_id not in users:
+        abort(404, message="Could not find user...")
+
+def abort_if_user_exists(user_id):
+    if user_id in users:
+        abort(409, message="User already exists with that ID...")
+
+def abort_if_room_not_exists(room_id):
+    if room_id not in rooms:
+        abort(404, message="Could not find room...")
+
+def abort_if_room_exists(room_id):
+    if room_id in rooms:
+        abort(409, message="Room already exists with that ID...")
+
+
+class User(Resource):
+    def get(self, user_id=None):
+        if user_id is None:
+            return users, 200
+        else:
+            abort_if_user_not_exists(user_id)
+            return users[user_id], 200
+
+    def post(self, user_id):
+        abort_if_user_exists(user_id)
+        args = user_post_args.parse_args()
+        users[user_id] = args
+        return users[user_id], 201
+        
+    def delete(self, user_id):
+        abort_if_user_not_exists(user_id)
+        del users[user_id]
+        return '', 204
+
+
+class Room(Resource):
+    def get(self, room_id=None):
+        if room_id is None:
+            return rooms, 200
+        else:
+            abort_if_room_not_exists(room_id)
+            return rooms[room_id], 200
+
+    def post(self, room_id):
+        abort_if_room_exists(room_id)
+        args = room_post_args.parse_args()
+        rooms[room_id] = args
+        return rooms[room_id], 201
+
+
+class RoomUser(Resource):
+    def get(self, room_id):
+        abort_if_room_not_exists(room_id)
+        message = "Users in room number " + str(room_id) + ":"
+        for id in roomusers[room_id]["user_id"]:
+            message += " "
+            message += str(id)
+        return message, 200
+    def post(self, room_id):
+        abort_if_room_not_exists(room_id)
+        args = room_user_post_args.parse_args()
+        roomusers[room_id] = args
+
+
+        return args, 201
+
+
+class Message(Resource):
+    def get(self, room_id):
+        return
+
+    def post(self, room_id, user_id):
+        return
+
+
+api.add_resource(User, "/api/user/<int:user_id>", "/api/users")
+api.add_resource(Room, "/api/rooms", "/api/room/<int:room_id>", "/api/rooms")
+api.add_resource(RoomUser, "/api/room/<int:room_id>/users")
+#api.add_resource(Message, "/api/room/<int:room_id>/messages", "/api/room/<int:room_id>/<int:user_id>/messages")
+
+
+@app.route('/')
+def index():
+    return "SHITTY OBLIG 2"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
