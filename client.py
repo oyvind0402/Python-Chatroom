4f6e50497cde86688c5d74c227e830a7c7af3dc5
anafvana <anafvana@gmail.com>
2021-04-11 21:39:02 +0200

    Added JSON check for querier, catching abrupt quit, added commands to chatrooms, user automatically deleted on quit.

--- client.py
+++ client.py
@@ -1,149 +1,198 @@
-import json
-import random
-import sys
+from threading import Timer
 
 import requests
-from flask import jsonify
-
-rooms = [
-    {"roomid": "a room",
-     "userlist": ["user1", "user2", "user3"],
-     "message_list": ["user1: hello", "user2: hello"]
-     },
-    {"roomid": 1,
-     "userlist": ["user1", "user2", "user3"],
-     "message_list": ["user1: hello", "user2: hello"]},
-]
+import time
+import sys
+import random
 
 
-id = 2
-userList = []
-message_list = []
-room = {"roomid" : id, "userlist": [], "message_list": []}
-rooms.append(room)
-print(json.dumps(rooms))
-print(rooms[0]["userlist"])
+print("----------------------------------------------")
+print("Welcome to the electric boogaloo API chatroom!")
+print("----------------------------------------------")
 
-rooms[0]["userlist"].append("user4")
-
-sys.exit()
-
-print(json.dumps(rooms))
-sys.exit()
+#TODO Bots
 
 BASE = "http://127.0.0.1:5000/api/"
 
-chatbots = ["bot1", "bot2", "bot3", "bot4"]
-picked_chatbot = random.choice(chatbots)
 
-# just so we can make a lot of users
-n = random.randint(0, 5000)
-
-# post_user = BASE + "user/", {"username": str(n)}
-# print(post_user)
-print("User picked " + str(n))
-response = requests.post(BASE + "user/" + str(n))
-print(response.json())
-# Legge til feilhåndtering dersom userid allerede finnes
-
-print("RESULT FROM GET ALL USERS")
-response = requests.get(BASE + "users")
-print(response.json())
-
-# ALT SOM STÅR UNDER MÅ BARE INGONERES ;)
-
-print("Welcome ", n, " your user has been created.")
-
-print()
-print(BASE + "rooms/")
-response = requests.get(BASE + "rooms")
-print(response.json())
-
-if response.json() == []:
-    will_join = "n"
-else:
-    print("Do you want to join one of the following rooms? [y/n]")
-    will_join = input()
-
-if will_join == "y":
-    print("good")
-else:
-    print("No is selected. Do you wish to create a room [y/n]")
-    create_room = input()
-    if create_room == "y":
-        print("Good what do you want to call your room?")
-        name = input()
-        response = requests.post(BASE + "room/" + name)
-        print(response.json() + " is created")
-    else:
-        print("That is to bad too hear. If you don't want to join or create a room. The program will be closed.")
-        print("Feel free to try again")
+while True:
+    try:
+        username = input("Type in your username: ")
+        response = requests.post(BASE + "user/" + username)
+        if response.status_code == 201:
+            print("User created with username: " + username)
+            break
+        elif response.status_code == 409:
+            print("A user already exists with that username, try another name.")
+            continue
+    except (EOFError, KeyboardInterrupt):
+        print(" You have chosen to end the chat and that is your loss")
+        sys.exit()
+    except TimeoutError:
+        print(" You're slow. Bye.")
+        sys.exit()
+    except EOFError:
+        print(" Oh no! Something's wrong. Bye :'(")
         sys.exit()
 
-""""
-Bruker er laget
-Sjekke om det finnes rom
-Deretter må den joines et rom
-Når i et rom. mulighet til å sende melding
-Og thread som sjekkerom det har kommet nye meldinger
 
-"""
+print("Thanks for joining the server! Type --help for a list of commands.")
 
-""""
-først en bruker
-Deretter må den joines et rom
-Når i et rom. mulighet til å sende melding
-Og thread som sjekkerom det har kommet nye meldinger
+def def_pass():
+    pass
 
-"""
-"""""
-print("-------------------------------------------------------")
-print("RESULT FROM POST A USER")
-response = requests.post(BASE + "user/0", {"username": "oyvind91", "usertype": "admin"})
-print(response.json())
+def chatroom(room_id):
+    room_id = str(room_id)
+    in_room = True
+    message_input = ""
+    print(f"You are now in the room {room_id}")
+    print("For help, type '--help'.")
 
-print("-------------------------------------------------------")
-print("RESULT FROM POST A USER")
-response = requests.post(BASE + "user/1", {"username": "someone", "usertype": "regular"})
-print(response.json())
-"""""
+    while in_room:
+        # timeout = 5.0
+        # timer_thread = Timer(timeout, def_pass)
+        # timer_thread.start()
+        # try:
+        message_input = input(username + " to room " + room_id + ": ")
+        # timer_thread.cancel()
+        # print("timer cancelled")
 
-print("-------------------------------------------------------")
-print("RESULT FROM GET A USER")
-response = requests.get(BASE + "user/0")
-print(response.json())
+        if message_input == "--exit":
+            print(f"----- You have exited room {room_id} ------")
+            print("You are back in the main terminal.")
+            break
+        elif message_input== "--help":
+             print("Type '--help' to show this help prompt\nType '--showmessages' to show all messages in this chatroom\nType '--exit' to leave the chatroom and go back to the main terminal.")
+        elif message_input == "--showmessages":
+            response = requests.get(BASE + "room/" + str(room_id) + "/user/" + username + "/messages", {"username": username})
+            print(response.json())
+        else:
+            response = requests.put(BASE + "room/" + str(room_id) + "/user/" + username + "/message/" + message_input, {"username": username})
 
-print("-------------------------------------------------------")
-print("RESULT FROM GET A USER")
-response = requests.get(BASE + "user/1")
-print(response.json())
 
-print("-------------------------------------------------------")
-print("RESULT FROM GET ALL USERS")
-response = requests.get(BASE + "users")
-print(response.json())
+#TODO Get one room only
+#TODO Get all room user for one room
+#TODO Be able to request other users
 
-print("-------------------------------------------------------")
-print("RESULT FROM POST A ROOM")
-response = requests.post(BASE + "room/0", {"roomname": "ObligWork"})
-print(response.json())
-
-print("-------------------------------------------------------")
-print("RESULT FROM POST THE SAME ROOM_ID")
-response = requests.post(BASE + "room/0", {"roomname": "ObligWork"})
-print(response.json())
-
-print("-------------------------------------------------------")
-print("RESULT FROM POST A ROOM")
-response = requests.post(BASE + "room/1", {"roomname": "PortfolioWork"})
-print(response.json())
-
-print("-------------------------------------------------------")
-print("RESULT FROM GET A ROOM")
-response = requests.get(BASE + "room/0")
-print(response.json())
-
-print("-------------------------------------------------------")
-print("RESULT FROM GET ALL ROOMS")
-response = requests.get(BASE + "rooms")
-print(response.json())
+while True:
+    try:
+        message = input(f'{username}: ')
+        if message == '--help':
+            print("Type '--create' to create a chat room\nType '--help' to show this help prompt\nType '--showrooms' " + 
+            "to show all rooms\nType '--start' to join a chatroom to start typing messages\nType '--join' to join a chatroom\n" +
+            "Type '--showmessages' to show all messages for a specific chatroom\nType '--showmymessages' to show all your messages for a specific chatroom.\nType '--exit' to leave the program. This will delete your user.")
+        elif message == '--create':
+            while True:
+                roomname = input("Type in the roomname: ")
+                roomid = (hash(roomname) % 256)
+                response = requests.post(BASE + "room/" + str(roomid), {"roomname": roomname, "username": username})
+                if response.status_code == 201:
+                    print("Chat room called " + roomname + " created.")
+                    joinmessage = input("Do you wish to join the room you just created?: ")
+                    joinmessage = joinmessage.lower()
+                    if joinmessage == 'y' or joinmessage == 'yes':
+                        response = requests.put(BASE + "room/" + str(roomid) + "/user/" + username, {"username": username})
+                        print("Joined " + roomname)
+                        chatroom(roomid)
+                        break
+                    elif joinmessage == 'n' or joinmessage == 'no':
+                        break
+                elif response.status_code == 409:
+                    print("A room already exists with that roomname, try again.")
+                    continue
+                elif response.status_code == 404:
+                    print("Wrong syntax for the roomname")
+        elif message == '--showrooms':
+            response = requests.get(BASE + "rooms", {"username": username})
+            print("Rooms:")
+            print(response.json())
+        elif message == '--join':
+            roommsg = input("If you want to see a list of rooms to join type '--show': ")
+            if roommsg == '--show':
+                response = requests.get(BASE + "rooms", {"username": username})
+                print("Rooms:")
+                print(response.json())
+            else:
+                print("You decided not to show rooms.")
+            roomchoice = input("Type the room_id of the room you wish to join: ")
+            response = requests.put(BASE + "room/" + roomchoice + "/user/" + username, {"username": username})
+            if response.status_code == 201:
+                print(username + " successfully joined room " + roomchoice)
+                chatroom(roomchoice)
+            elif response.status_code == 404:
+                print("Couldnt join room " + roomchoice + ". No room with that ID.")
+            else:
+                print("Something went wrong joining room " + roomchoice)
+        elif message == '--start':
+            roommsg = input("If you want to see a list of rooms to join type '--show': ")
+            if roommsg == '--show':
+                response = requests.get(BASE + "rooms", {"username": username})
+                print("Rooms:")
+                print(response.json())
+            else:
+                print("You decided not to show rooms.")
+            roomchoice = input("Type the room_id of the room you wish to join: ")
+            response = requests.get(BASE + "room/" + roomchoice + "/user/" + username, {"username": username})
+            if response.status_code == 201:
+                print(username + " successfully joined room " + roomchoice)
+                while True:
+                    print("Type '--leave' to leave the room, you'll still be registered to the room.")
+                    usermessage = input(f"{username}: ")
+                    if usermessage == '--leave':
+                        print("Leaving room " + roomchoice)
+                        break
+                    else:
+                        response = requests.put(BASE + "room/" + roomchoice + "/user/" + username + "/message/" + usermessage, {"username": username})
+                        if response.status_code != 201 :
+                            print("Couldnt send message, something went wrong.")             
+            elif response.status_code == 404:
+                print("Couldnt join room " + roomchoice + ". No room with that ID.")
+            else:
+                print("Something went wrong joining room " + roomchoice)
+        elif message == '--showmymessages':
+            roommsg = input("If you want to see a list of rooms to check for messages in type '--show': ")
+            if roommsg == '--show':
+                response = requests.get(BASE + "rooms", {"username": username})
+                print("Rooms:")
+                print(response.json())
+            else:
+                print("You decided not to show rooms.")
+            roomchoice = input("Type the room_id of the room you wish to see the messages in: ")
+            response = requests.get(BASE + "room/" + roomchoice + "/messages", {"username": username})
+            print("Messages in room " + roomchoice + " from " + username + ":")
+            print(response.json())
+        elif message == '--showmessages':
+            roommsg = input("If you want to see a list of rooms to check for messages in type '--show': ")
+            if roommsg == '--show':
+                response = requests.get(BASE + "rooms", {"username": username})
+                print("Rooms:")
+                print(response.json())
+            else:
+                print("You decided not to show rooms.")
+            roomchoice = input("Type the room_id of the room you wish to see the messages in: ")
+            response = requests.get(BASE + "room/" + roomchoice + "/user/" + username + "/messages", {"username": username})
+            print("Messages in room " + roomchoice + ":")
+            print(response.json())
+        elif message == '--exit':
+            print("Are you sure you want to exit? This will DELETE your user!")
+            answer = input("Y(es) or N(o): ")
+            answer = answer.lower()
+            if answer == "y" or answer == "yes":
+                response = requests.delete(BASE + "user/" + username, {"username": username})
+                sys.exit("You have exited the program. Goodbye.")
+            elif answer == "n" or answer == "no":
+                continue
+            else:
+                print("Invalid input. You will be taken back to the main terminal.")
+    except (EOFError, KeyboardInterrupt):
+        print("\nYou have chosen to leave the program and that is your loss. \nYour user is being deleted as revenge.")
+        response = requests.delete(BASE + "user/" + username, {"username": username})
+        sys.exit()
+    except TimeoutError:
+        print("\nYou're slow. Bye. \nPS: Your user has been deeeeeleeeeeeteeeeeed.")
+        response = requests.delete(BASE + "user/" + username, {"username": username})
+        sys.exit()
+    except EOFError:
+        print("\nOh no! Something's wrong. Bye :'( \nPS: Your user has been erased from our hearts.")
+        response = requests.delete(BASE + "user/" + username, {"username": username})
+        sys.exit()
--- main.py
+++ main.py
@@ -1,143 +1,245 @@
+import json
+import socket
+import sys
+import threading
+import time
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
-user_post_args = reqparse.RequestParser()
-# user_post_args.add_argument("userid", type=str, help="Userid is required...", required=True)
-# user_post_args.add_argument("usertype", type=str, help="Invalid syntax for usertype...", required=False)
+user_get_args = reqparse.RequestParser()
+user_get_args.add_argument("username", type=str, help="Username is required", required=True)
+
+user_delete_args = reqparse.RequestParser()
+user_delete_args.add_argument("username", type=str, help="Username is required", required=True)
 
 room_post_args = reqparse.RequestParser()
-room_post_args.add_argument("roomname", type=str, help="Roomname is required...", required=True)
-# room_post_args.add_argument("room_users", type=str,
-# help="Invalid syntax for adding a user to the room...", required=False, action="append")
-room_get_args = reqparse.RequestParser()
-room_get_args.add_argument("roomid", type=str, help="Roomname is required...", required=True)
-room_put_args = reqparse.RequestParser()
-room_put_args.add_argument("roomid", type=str, help="Roomname is required...", required=True)
-room_put_args.add_argument("user_id", type=str, help="User ID is required...", required=True)
+room_post_args.add_argument("roomname", type=str, help="Roomname is required.", required=True)
+room_post_args.add_argument("username", type=str, help="Username is required", required=True)
 
-# room_user_post_args = reqparse.RequestParser()
-# room_user_post_args.add_argument("user_id", type=str, help="User id is required...", required=True)
-# room_user_post_args.add_argument("username", type=str, help="Username is required...", required=True)
+room_get_args = reqparse.RequestParser()
+room_get_args.add_argument("username", type=str, help="Username is required", required=True)
+
+room_user_put_args = reqparse.RequestParser()
+room_user_put_args.add_argument("username", type=str, help="Username is required", required=True)
+
+room_user_get_args = reqparse.RequestParser()
+room_user_get_args.add_argument("username", type=str, help="Username is required", required=True)
+
+message_put_args = reqparse.RequestParser()
+message_put_args.add_argument("username", type=str, help="Username is required", required=True)
+#TODO Check this mothafucka
+#message_put_args.add_argument("message", type=str, action="append", help="Message cannot be empty", required=True)
+
+message_get_args = reqparse.RequestParser()
+message_get_args.add_argument("username", type=str, help="Username is required...", required=False)
 
 users = []
-rooms = []
-messages = {}
+rooms = {}
+
+'''
+{
+    0 : {
+        'roomname': name,
+        'userlist' : [user0, user1]
+        'message_list':[msg, msg]
+    }
+}
+'''
 
 
-def abort_if_user_not_exists(user_id):
-    if user_id not in users:
-        abort(404, message="Could not find user...")
+# Create post args for messages
+def abort_if_user_not_exists(username):
+    if username not in users:
+        abort(404, message=f"Could not find user \"{username}\"")
 
 
-def abort_if_user_exists(user_id):
-    if user_id in users:
-        abort(409, message="User already exists with that ID...")
+def abort_if_user_exists(username):
+    if username in users:
+        abort(409, message=f"User already exists with ID \"{username}\"")
+
+def abort_if_querier_does_not_match_user(querier, username):
+    if querier != username:
+        abort(401, message=f"You can only perform this action for your own user.")
 
 
 def abort_if_room_not_exists(room_id):
     if room_id not in rooms:
-        abort(404, message="Could not find room...")
+        abort(404, message=f"Could not find room {room_id}")
 
 
 def abort_if_room_exists(room_id):
     if room_id in rooms:
-        abort(409, message="Room already exists with that ID...")
+        abort(409, message=f"Room already exists with ID {room_id}")
 
+
+def abort_if_room_empty(room_id):
+    if len(rooms[room_id]["userlist"]) == 0:
+        abort(404, message=f"Could not find any users for room {room_id}.")
+
+
+def abort_if_roomuser_not_exists(room_id, username):
+    if username not in rooms[room_id]["userlist"]:
+        abort(404, message=f"User \"{username}\" was not found in room {room_id}")
+
+
+def abort_if_roomuser_exists(room_id, username):
+    if username in rooms[room_id]["userlist"]:
+        abort(409, message=f"User \"{username}\" is already in room {room_id}")
+
+
+def abort_if_message_list_empty(room_id):
+    if len(rooms[room_id]["message_list"]) == 0:
+        abort(404, message=f"Could not find any messages for room {room_id}")
+
+def abort_if_querier_not_in_room(querier, room_id):
+    if querier not in rooms[room_id]['userlist']:
+        abort(401, message=f"You must be part of this room to post to it.")
 
 class User(Resource):
-    def get(self, user_id=None):
-        if user_id is None:
+    def get(self, username=None):
+        querier = user_get_args.parse_args()
+        abort_if_user_not_exists(querier['username'])
+
+        if username == None:
             return users, 200
         else:
-            abort_if_user_not_exists(user_id)
-            return users[user_id], 200
+            abort_if_user_not_exists(username)
+            if username in users:
+                return username, 200
 
-    # def post(self, user_id):
-    #    abort_if_user_exists(user_id)
-    #    args = user_post_args.parse_args()
-    #    users[user_id] = args
-    #    return users[user_id], 201
+    def post(self, username):
+        abort_if_user_exists(username)
+        users.append(username)
+        return username, 201
 
-    def post(self, user_id):
-        abort_if_user_exists(user_id)
-        # args = user_post_args.parse.args()
-        users.append(user_id)
-        return user_id, 201
-
-    def delete(self, user_id):
-        abort_if_user_not_exists(user_id)
-        del users[user_id]
+    def delete(self, username):
+        querier = user_delete_args.parse_args()
+        abort_if_querier_does_not_match_user(querier['username'], username)
+        abort_if_user_not_exists(username)
+        users.remove(username)
+        for room_id in rooms:
+            if username in rooms[room_id]['userlist']:
+                rooms[room_id]['userlist'].remove(username)
         return '', 204
 
 
 class Room(Resource):
-    user_list = []
+    def get(self, room_id=None):
+        querier = room_get_args.parse_args()
+        abort_if_user_not_exists(querier['username'])
 
-    def get(self, room_name):
-        if room_name is None:
-            return 404
+        if room_id is None:
+            formatted_rooms = {}
+            for key in rooms:
+                formatted_rooms[key] = {"roomname": rooms[key]['roomname'], "userlist": rooms[key]['userlist']}
+                print(type(formatted_rooms))
+                print(type(formatted_rooms[key]))
+            return formatted_rooms, 200
         else:
-            abort_if_room_not_exists(room_name)
-            return room_name, 200
+            room_id = str(room_id)
+            abort_if_room_not_exists(room_id)
+            return rooms[room_id], 200
 
-    def post(self, room_name):
-        abort_if_room_exists(room_name)
-        rooms.append(room_name)
-        # abort_if_room_exists(room_id)
-        # args = room_post_args.parse_args()
-        # rooms[room_id] = args
+    def post(self, room_id):
+        querier = room_post_args.parse_args()
+        abort_if_user_not_exists(querier['username'])
 
-        return room_name, 201
-
-    def put(self, room_name, user_id):
+        room_id = str(room_id)
+        abort_if_room_exists(room_id)
         args = room_post_args.parse_args()
 
-    def adduser(self, username):
-        user_list.append
-
-class Rooms(Resource):
-    def get(self):
-        return rooms, 200
+        room = {"roomname": args["roomname"], "userlist": [], "message_list": []}
+        rooms[room_id] = room
+        return room_id, 201
 
 
-"""""
 class RoomUser(Resource):
-    def get(self, room_id):
-        return rooms[room_id]["room_users"]
+    def get(self, room_id, username=None):
+        querier = room_user_get_args.parse_args()
+        abort_if_user_not_exists(querier['username'])
 
-    def post(self, room_name):
-        args = room_user_post_args()
-        rooms[room_id]["room_users"] = args
-        return rooms[room_id]["room_users"], 201
-"""""
+        room_id = str(room_id)
 
-# class Message(Resource):
-#     def get(self, room_id):
-#         return
+        abort_if_room_not_exists(room_id)
+        if username is None:
+            return rooms[room_id]["userlist"], 201
+        else:
+            abort_if_room_empty(room_id)
+            abort_if_user_not_exists(username)
+            abort_if_roomuser_not_exists(room_id, username)
+            return username + " is in the room " + str(room_id), 201
 
-# def post(self, room_id, user_id):
-#     return
+    def put(self, room_id, username):
+        querier = room_user_put_args.parse_args()
+        abort_if_querier_does_not_match_user(querier['username'], username)
+        room_id = str(room_id)
+
+        abort_if_user_not_exists(username)
+        abort_if_room_not_exists(room_id)
+        abort_if_roomuser_exists(room_id, username)
+        rooms[room_id]["userlist"].append(username)
+        return username + " is connected to " + str(room_id), 201
 
 
-api.add_resource(User, "/api/user/<string:user_id>", "/api/users")
-api.add_resource(Room, "/api/room/<string:room_name>")
-api.add_resource(Rooms, "/api/rooms")
+class Message(Resource):
+    def get(self, room_id, username=None):
+        room_id = str(room_id)
+        
+        querier = message_get_args.parse_args()
+        print(querier)
+        print(querier['username'])
+        abort_if_querier_not_in_room(str(querier['username']), room_id)
+
+        args = message_get_args.parse_args()
+        if username is not None:
+            abort_if_room_not_exists(room_id)
+            abort_if_roomuser_not_exists(room_id, username)
+            abort_if_message_list_empty(room_id)
+            return rooms[room_id]["message_list"], 200
+        else:
+            username = args["username"]
+            abort_if_room_not_exists(room_id)
+            abort_if_roomuser_not_exists(room_id, username)
+            abort_if_message_list_empty(room_id)
+            usermessages = []
+            for obj in rooms[room_id]["message_list"]:
+                if obj["username"] == username:
+                    usermessages.append(obj["message"])
+            return usermessages, 200 
+
+    def put(self, room_id, username, message):
+        querier = message_put_args.parse_args()
+        abort_if_querier_does_not_match_user(querier['username'], username)
+        
+        room_id = str(room_id)
+
+        abort_if_room_not_exists(room_id)
+        abort_if_roomuser_not_exists(room_id, username)
+        user_message = {"username": username, "message": message}
+        rooms[room_id]["message_list"].append(user_message)
+        return message, 201
 
 
-# api.add_resource(RoomUser, "/api/room/<int:room_id>/users")
-
-
-# api.add_resource(Message, "/api/room/<int:room_id>/messages")
-# api.add_resource(Message, "/api/room/<int:room_id>/<int:user_id>/messages")
+api.add_resource(User, "/api/user/<string:username>", "/api/users")
+api.add_resource(Room, "/api/room/<int:room_id>", "/api/rooms")
+api.add_resource(RoomUser, "/api/room/<int:room_id>/user/<string:username>", "/api/room/<int:room_id>/users")
+api.add_resource(Message, "/api/room/<int:room_id>/user/<string:username>/message/<string:message>",
+                 "/api/room/<int:room_id>/user/<string:username>/messages", "/api/room/<int:room_id>/messages")
 
 
 @app.route('/')
 def index():
-    return "SHITTY OBLIG 2"
+    return "OBLIG 2"
 
+# api_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 if __name__ == "__main__":
+    # ip = "127.0.0.2"
+    # port = 5001
+    # api_server.connect((ip, port))
+    # message = "api"
+    # api_server.send(message.encode())
     app.run(debug=True)
